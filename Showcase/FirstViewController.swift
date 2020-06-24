1d1c0d6d576f2d2b28f51a1d9526b1b53227f1fc
law-lin <lawrencelin101@gmail.com>
2020-06-24 11:44:16 -0400

    Initial Commit

---
+++ Showcase/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Showcase
+//
+//  Created by Lawrence Lin on 6/24/20.
+//  Copyright © 2020 Lawrence Lin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
